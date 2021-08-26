335cbb0599d45f9c294e12e1b95dda5d8f4df9b6
Bryce Derriso <b.derriso@gmail.com>
2021-08-26 14:20:48 -0500

    Add todo.

--- datalake/src/test/java/com/sequenceiq/datalake/events/DatalakeUrlParserTest.java
+++ datalake/src/test/java/com/sequenceiq/datalake/events/DatalakeUrlParserTest.java
@@ -3,6 +3,7 @@ package com.sequenceiq.datalake.events;
 import org.junit.jupiter.api.BeforeEach;
 
 class DatalakeUrlParserTest {
+    //todo: implement me, https://jira.cloudera.com/browse/CB-13786
     @BeforeEach
     void setUp() {
     }
